9bef9259cb6503e540a546077cf31e24c349e994
Uriel Rodriguez <u.e.rodriguez@gmail.com>
2020-01-16 21:04:11 +0000

    Automatically backed up by Learn

--- lib/array_methods.rb
+++ lib/array_methods.rb
@@ -10,30 +10,30 @@ end
 def find_max_value(array)
     current = array
     hold = []
-    while current.length > 1 {
-        current.length.times { |i|
+    while current.length > 1 do
+        current.length.times do |i|
             if current[i] >= current[-1] && hold.include?(current[i]) == false
                 hold.push(current[i])
             end
-        }
+        end
         hold.reverse!
         current = hold.pop(hold.length)
-    }  
+    end    
     return current[0]
 end
 
 def find_min_value(array)
     current = array
     hold = []
-    while current.length > 1 {
-        current.length.times do |i| {
+    while current.length > 1 do
+        current.length.times do |i|
             if current[i] <= current[-1] && hold.include?(current[i]) == false
                 hold.push(current[i])
             end
-        }
+        end
         hold.reverse!
         current = hold.pop(hold.length)
-    }   
+    end    
     return current[0]
 end
 
